1a2861492d02689ccdc2739b7dd98bb3dcce1a20
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-04-12 11:49:41 +0300

    added gmail login

---
+++ src/com/omnipasteapp/omni/GoogleLoginDialogFragment.java
@@ -0,0 +1,66 @@
+package com.omnipasteapp.omni;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class GoogleLoginDialogFragment extends DialogFragment implements DialogInterface.OnClickListener {
+
+    public interface GoogleLoginDialogListener {
+        public void onAccountSelected(Account account);
+    }
+
+    public static final String TAG = "GoogleLoginDialogFragment";
+
+    private GoogleLoginDialogListener _listener;
+    private Account[] _accounts;
+
+    @Override
+    public void onAttach(Activity activity){
+        super.onAttach(activity);
+
+        _accounts = getAccounts();
+
+        if(activity instanceof GoogleLoginDialogListener){
+            _listener = (GoogleLoginDialogListener) activity;
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstance){
+        final int size = _accounts.length;
+        String[] names = new String[size];
+        for (int i = 0; i < size; i++) {
+            names[i] = _accounts[i].name;
+        }
+
+        return new AlertDialog.Builder(getActivity())
+                .setTitle("Select a Google account")
+                .setItems(names, this)
+                .create();
+    }
+
+    public void setListener(LoginActivity listener) {
+        _listener = listener;
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+        if(_listener != null){
+            _listener.onAccountSelected(_accounts[i]);
+        }
+    }
+
+    private Account[] getAccounts(){
+        AccountManager accountManager = AccountManager.get(getActivity().getApplicationContext());
+
+        final Account[] accounts = accountManager.getAccountsByType("com.google");
+
+        return accounts;
+    }
+}
--- src/com/omnipasteapp/omni/LoginActivity.java
+++ src/com/omnipasteapp/omni/LoginActivity.java
@@ -1,13 +1,13 @@
 package com.omnipasteapp.omni;
 
+import android.accounts.Account;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import com.omnipasteapp.omni.core.ClipboardService;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements GoogleLoginDialogFragment.GoogleLoginDialogListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,18 +17,26 @@ public class LoginActivity extends Activity {
 
     @SuppressWarnings("UnusedParameters")
     public void loginClicked(View view) {
-        EditText editText = (EditText) findViewById(R.id.login_email);
-        String email = editText.getText().toString();
+        GoogleLoginDialogFragment dialog = new GoogleLoginDialogFragment();
+        dialog.setListener(this);
+        dialog.show(getFragmentManager(), GoogleLoginDialogFragment.TAG);
+    }
 
+    @Override
+    public void onAccountSelected(Account account) {
+        onSuccess(account.name);
+    }
+
+    private void onSuccess(String channel){
         // start the clipboard service
         Intent intent = new Intent(this, ClipboardServiceCommandReceiver.class);
-        intent.putExtra(ClipboardService.CHANNEL_NAME, email);
+        intent.putExtra(ClipboardService.CHANNEL_NAME, channel);
         intent.setAction(ClipboardService.START);
         sendBroadcast(intent);
 
         // navigate to main activity
         Intent activity_intent = new Intent(this, MainActivity.class);
-        activity_intent.putExtra(ClipboardService.CHANNEL_NAME, email);
+        activity_intent.putExtra(ClipboardService.CHANNEL_NAME, channel);
         startActivity(activity_intent);
     }
 }
