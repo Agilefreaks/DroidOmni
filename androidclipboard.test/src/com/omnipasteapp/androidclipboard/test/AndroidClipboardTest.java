824e61895e20d90268ae5aab06baa26131230206
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-05-26 18:03:52 +0300

    implemented android clipboard

--- androidclipboard.test/src/com/omnipasteapp/androidclipboard/test/AndroidClipboardTest.java
+++ androidclipboard.test/src/com/omnipasteapp/androidclipboard/test/AndroidClipboardTest.java
@@ -1,21 +1,98 @@
 package com.omnipasteapp.androidclipboard.test;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import com.omnipasteapp.androidclipboard.AndroidClipboard;
+import com.omnipasteapp.omnicommon.interfaces.ICanReceiveData;
+import com.omnipasteapp.omnicommon.interfaces.IClipboardData;
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
 
 @RunWith(RobolectricTestRunner.class)
 public class AndroidClipboardTest {
-  @Before
-  public void setUp() {
-  }
+    private Context mockContext;
+    private AndroidClipboard subject;
 
-  @Test
-  public void someTest() {
-    assertThat(true, is(false));
-  }
+    @Before
+    public void setUp() {
+        mockContext = mock(Context.class);
+        subject = new AndroidClipboard();
+        subject.setContext(mockContext);
+    }
+
+    @Test
+    public void initializeReturnsNewThread(){
+        Thread result = subject.initialize();
+
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void runCallsContextGetClibpoardService(){
+        ClipboardManager mockClipboardManager = mock(ClipboardManager.class);
+        when(mockContext.getSystemService(eq(Context.CLIPBOARD_SERVICE)))
+                .thenReturn(mockClipboardManager);
+
+        subject.run();
+
+        verify(mockContext).getSystemService(eq(Context.CLIPBOARD_SERVICE));
+    }
+
+    @Test
+    public void runCallsAddPrimaryClipChangedListener(){
+        ClipboardManager mockClipboardManager = mock(ClipboardManager.class);
+        when(mockContext.getSystemService(eq(Context.CLIPBOARD_SERVICE)))
+                .thenReturn(mockClipboardManager);
+
+        subject.run();
+
+        verify(mockClipboardManager).addPrimaryClipChangedListener(eq(subject));
+    }
+
+    @Test
+    public void disposeCallsRemovePrimaryClipChangedListener(){
+        ClipboardManager mockClipboardManager = mock(ClipboardManager.class);
+        when(mockContext.getSystemService(eq(Context.CLIPBOARD_SERVICE)))
+                .thenReturn(mockClipboardManager);
+
+        subject.dispose();
+
+        verify(mockClipboardManager).removePrimaryClipChangedListener(eq(subject));
+    }
+
+    @Test
+    public void onPrimaryClipChangedCallsReceiverDataReceived(){
+        ClipData mockClipData = mock(ClipData.class);
+        when(mockClipData.getItemCount()).thenReturn(1);
+        when(mockClipData.getItemAt(eq(0))).thenReturn(new ClipData.Item("test"));
+        ClipboardManager mockClipboardManager = mock(ClipboardManager.class);
+        when(mockClipboardManager.hasPrimaryClip()).thenReturn(true);
+        when(mockClipboardManager.getPrimaryClip()).thenReturn(mockClipData);
+        when(mockContext.getSystemService(eq(Context.CLIPBOARD_SERVICE)))
+                .thenReturn(mockClipboardManager);
+        ICanReceiveData mockDataReceiver = mock(ICanReceiveData.class);
+        subject.addDataReceiver(mockDataReceiver);
+
+        subject.onPrimaryClipChanged();
+
+        verify(mockDataReceiver).dataReceived(any(IClipboardData.class));
+    }
+
+    @Test
+    public void putDataAlwaysCallsClipboardManagerSetPrimaryClip(){
+        ClipboardManager mockClipboardManager = mock(ClipboardManager.class);
+        when(mockContext.getSystemService(eq(Context.CLIPBOARD_SERVICE)))
+                .thenReturn(mockClipboardManager);
+
+        subject.putData("test");
+
+        verify(mockClipboardManager).setPrimaryClip(any(ClipData.class));
+    }
 }
--- androidclipboard/src/com/omnipasteapp/androidclipboard/AndroidClipboard.java
+++ androidclipboard/src/com/omnipasteapp/androidclipboard/AndroidClipboard.java
@@ -1,36 +1,88 @@
 package com.omnipasteapp.androidclipboard;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import com.google.inject.Inject;
+import com.omnipasteapp.omnicommon.ClipboardData;
 import com.omnipasteapp.omnicommon.interfaces.ICanReceiveData;
 import com.omnipasteapp.omnicommon.interfaces.ILocalClipboard;
 
-public class AndroidClipboard implements ILocalClipboard {
-  @Inject
-  Context context;
+import java.util.ArrayList;
 
-  @Override
-  public void addDataReceiver(ICanReceiveData dataReceiver) {
-    //To change body of implemented methods use File | Settings | File Templates.
-  }
+public class AndroidClipboard implements ILocalClipboard, Runnable, ClipboardManager.OnPrimaryClipChangedListener {
 
-  @Override
-  public void removeDataReceive(ICanReceiveData dataReceiver) {
-    //To change body of implemented methods use File | Settings | File Templates.
-  }
+    private ArrayList<ICanReceiveData> dataReceivers;
 
-  @Override
-  public Thread initialize() {
-    return new Thread();
-  }
+    @Inject
+    Context context;
 
-  @Override
-  public void dispose() {
-    //To change body of implemented methods use File | Settings | File Templates.
-  }
+    public AndroidClipboard(){
+        dataReceivers = new ArrayList<ICanReceiveData>();
+    }
 
-  @Override
-  public void putData(String data) {
-    //To change body of implemented methods use File | Settings | File Templates.
-  }
+    @Override
+    public void addDataReceiver(ICanReceiveData dataReceiver) {
+        dataReceivers.add(dataReceiver);
+    }
+
+    @Override
+    public void removeDataReceive(ICanReceiveData dataReceiver) {
+        dataReceivers.remove(dataReceiver);
+    }
+
+    @Override
+    public void putData(String data) {
+        ClipboardManager manager = getClipboardManager();
+
+        manager.setPrimaryClip(ClipData.newPlainText("", data));
+    }
+
+    @Override
+    public Thread initialize() {
+        return new Thread(this);
+    }
+
+    @Override
+    public void run() {
+        ClipboardManager manager = getClipboardManager();
+
+        manager.addPrimaryClipChangedListener(this);
+    }
+
+    @Override
+    public void dispose() {
+        ClipboardManager manager = getClipboardManager();
+
+        manager.removePrimaryClipChangedListener(this);
+
+        dataReceivers.clear();
+    }
+
+    @Override
+    public void onPrimaryClipChanged() {
+        ClipboardManager manager = getClipboardManager();
+
+        if(!manager.hasPrimaryClip() || manager.getPrimaryClip().getItemCount() == 0) return;
+
+        String clip = manager.getPrimaryClip().getItemAt(0).getText().toString();
+
+        ClipboardData data = new ClipboardData(this, clip);
+
+        for(ICanReceiveData receiver : dataReceivers){
+            receiver.dataReceived(data);
+        }
+    }
+
+    public void setContext(Context context){
+        this.context = context;
+    }
+
+    public Context getContext(){
+        return context;
+    }
+
+    protected ClipboardManager getClipboardManager(){
+        return (ClipboardManager)context.getSystemService(Context.CLIPBOARD_SERVICE);
+    }
 }
