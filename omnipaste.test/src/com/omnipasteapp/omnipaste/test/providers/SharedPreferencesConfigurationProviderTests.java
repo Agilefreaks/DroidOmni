24fc0ede9fd3cab3d472d71907ca835e3ecb374a
calin <calinoiu.alexandru@agilefreaks.com>
2013-05-27 11:54:35 +0300

    op-86 udpates tests

--- omnipaste.test/src/com/omnipasteapp/omnipaste/test/providers/SharedPreferencesConfigurationProviderTests.java
+++ omnipaste.test/src/com/omnipasteapp/omnipaste/test/providers/SharedPreferencesConfigurationProviderTests.java
@@ -2,11 +2,18 @@ package com.omnipasteapp.omnipaste.test.providers;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import com.google.inject.AbstractModule;
+import com.google.inject.util.Modules;
 import com.omnipasteapp.omnipaste.providers.SharedPreferencesConfigurationProvider;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import roboguice.RoboGuice;
 
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
@@ -15,60 +22,79 @@ import static org.mockito.Mockito.*;
 
 @RunWith(RobolectricTestRunner.class)
 public class SharedPreferencesConfigurationProviderTests {
-    private Context mockContext;
-    private SharedPreferences mockSharedPreferences;
-    private SharedPreferencesConfigurationProvider subject;
+  @Mock
+  private Context mockContext;
+  @Mock
+  private SharedPreferences mockSharedPreferences;
 
-    @Before
-    public void Setup(){
-        mockContext = mock(Context.class);
-        mockSharedPreferences = mock(SharedPreferences.class);
-        when(mockContext.getSharedPreferences(anyString(), anyInt())).thenReturn(mockSharedPreferences);
-        subject = new SharedPreferencesConfigurationProvider();
-        subject.setContext(mockContext);
+  private SharedPreferencesConfigurationProvider subject;
+
+  public class TestModule extends AbstractModule {
+    @Override
+    protected void configure() {
+      bind(Context.class).toInstance(mockContext);
     }
+  }
 
-    @Test
-    public void getValueGetsSharedPreferencesInPrivateMode(){
-        subject.getValue("key");
+  @Before
+  public void Setup() {
+    MockitoAnnotations.initMocks(this);
+    when(mockContext.getSharedPreferences(anyString(), anyInt())).thenReturn(mockSharedPreferences);
 
-        verify(mockContext).getSharedPreferences(anyString(), eq(Context.MODE_PRIVATE));
-    }
+    RoboGuice
+        .setBaseApplicationInjector(Robolectric.application, RoboGuice.DEFAULT_STAGE, Modules.override(RoboGuice.newDefaultRoboModule(Robolectric.application))
+            .with(new TestModule()));
 
-    @Test
-    public void getValueCallsSharedPreferencesGetStringValue(){
-        subject.getValue("key");
+    subject = new SharedPreferencesConfigurationProvider();
+    RoboGuice.getInjector(Robolectric.application).injectMembers(subject);
+  }
 
-        verify(mockSharedPreferences).getString(eq("key"), anyString());
-    }
+  @After
+  public void tearDown() {
+    RoboGuice.util.reset();
+  }
 
-    @Test
-    public void setValueCallsSharedPreferencesEdit(){
-        SharedPreferences.Editor mockEditor = mock(SharedPreferences.Editor.class);
-        when(mockSharedPreferences.edit()).thenReturn(mockEditor);
+  @Test
+  public void getValueGetsSharedPreferencesInPrivateMode() {
+    subject.getValue("key");
 
-        subject.setValue("key", "value");
+    verify(mockContext).getSharedPreferences(anyString(), eq(Context.MODE_PRIVATE));
+  }
 
-        verify(mockSharedPreferences).edit();
-    }
+  @Test
+  public void getValueCallsSharedPreferencesGetStringValue() {
+    subject.getValue("key");
 
-    @Test
-    public void setValueCallsEditorPutString(){
-        SharedPreferences.Editor mockEditor = mock(SharedPreferences.Editor.class);
-        when(mockSharedPreferences.edit()).thenReturn(mockEditor);
+    verify(mockSharedPreferences).getString(eq("key"), anyString());
+  }
 
-        subject.setValue("key", "value");
+  @Test
+  public void setValueCallsSharedPreferencesEdit() {
+    SharedPreferences.Editor mockEditor = mock(SharedPreferences.Editor.class);
+    when(mockSharedPreferences.edit()).thenReturn(mockEditor);
 
-        verify(mockEditor).putString(eq("key"), eq("value"));
-    }
+    subject.setValue("key", "value");
 
-    @Test
-    public void setValueCallsCommit(){
-        SharedPreferences.Editor mockEditor = mock(SharedPreferences.Editor.class);
-        when(mockSharedPreferences.edit()).thenReturn(mockEditor);
+    verify(mockSharedPreferences).edit();
+  }
 
-        subject.setValue("key", "value");
+  @Test
+  public void setValueCallsEditorPutString() {
+    SharedPreferences.Editor mockEditor = mock(SharedPreferences.Editor.class);
+    when(mockSharedPreferences.edit()).thenReturn(mockEditor);
 
-        verify(mockEditor).commit();
-    }
+    subject.setValue("key", "value");
+
+    verify(mockEditor).putString(eq("key"), eq("value"));
+  }
+
+  @Test
+  public void setValueCallsCommit() {
+    SharedPreferences.Editor mockEditor = mock(SharedPreferences.Editor.class);
+    when(mockSharedPreferences.edit()).thenReturn(mockEditor);
+
+    subject.setValue("key", "value");
+
+    verify(mockEditor).commit();
+  }
 }
--- omnipaste/src/com/omnipasteapp/omnipaste/providers/SharedPreferencesConfigurationProvider.java
+++ omnipaste/src/com/omnipasteapp/omnipaste/providers/SharedPreferencesConfigurationProvider.java
@@ -5,39 +5,31 @@ import android.content.SharedPreferences;
 import com.google.inject.Inject;
 import com.omnipasteapp.omnicommon.interfaces.IConfigurationProvider;
 
-public class SharedPreferencesConfigurationProvider implements IConfigurationProvider{
+public class SharedPreferencesConfigurationProvider implements IConfigurationProvider {
+  public static final String SharedPreferenceKey = "omnipaste";
 
-    public static final String SharedPreferenceKey = "omnipaste";
+  @Inject
+  Context context;
 
-    @Inject
-    Context context;
+  @Override
+  public String getValue(String key) {
+    SharedPreferences preferences = getSharedPreferences();
 
-    @Override
-    public String getValue(String key) {
-        SharedPreferences preferences = getSharedPreferences();
+    return preferences.getString(key, null);
+  }
 
-        return preferences.getString(key, null);
-    }
+  @Override
+  public boolean setValue(String key, String value) {
+    SharedPreferences preferences = getSharedPreferences();
 
-    @Override
-    public boolean setValue(String key, String value) {
-        SharedPreferences preferences = getSharedPreferences();
+    SharedPreferences.Editor editor = preferences.edit();
 
-        SharedPreferences.Editor editor = preferences.edit();
+    editor.putString(key, value);
 
-        editor.putString(key, value);
+    return editor.commit();
+  }
 
-        return editor.commit();
-    }
-
-    public void setContext(Context context){
-        this.context = context;
-    }
-    public Context getContext(){
-        return context;
-    }
-
-    protected SharedPreferences getSharedPreferences(){
-        return context.getSharedPreferences(SharedPreferenceKey, Context.MODE_PRIVATE);
-    }
+  protected SharedPreferences getSharedPreferences() {
+    return context.getSharedPreferences(SharedPreferenceKey, Context.MODE_PRIVATE);
+  }
 }
