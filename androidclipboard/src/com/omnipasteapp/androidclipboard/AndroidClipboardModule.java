c43c5a47a8cbd117bec17b2317748fe927aecbfb
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-06-13 11:05:13 +0300

    op-109 added registrations to module

--- androidclipboard/src/com/omnipasteapp/androidclipboard/AndroidClipboardModule.java
+++ androidclipboard/src/com/omnipasteapp/androidclipboard/AndroidClipboardModule.java
@@ -1,7 +1,7 @@
 package com.omnipasteapp.androidclipboard;
 
-import android.content.ClipboardManager;
 import android.content.Context;
+import android.os.Build;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import com.omnipasteapp.omnicommon.interfaces.ILocalClipboard;
@@ -10,7 +10,14 @@ import roboguice.inject.SystemServiceProvider;
 public class AndroidClipboardModule extends AbstractModule {
   @Override
   protected void configure() {
-    bind(ILocalClipboard.class).to(AndroidClipboard.class).in(Singleton.class);
-    bind(ClipboardManager.class).toProvider(new SystemServiceProvider<ClipboardManager>(Context.CLIPBOARD_SERVICE));
+
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+      bind(ILocalClipboard.class).to(CompatibilityAndroidClipboard.class).in(Singleton.class);
+      bind(android.text.ClipboardManager.class).toProvider(new SystemServiceProvider<android.text.ClipboardManager>(Context.CLIPBOARD_SERVICE));
+      bind(ClipboardManagerWrapper.class).to(ClipboardManagerWrapper.class).in(Singleton.class);
+    } else {
+      bind(ILocalClipboard.class).to(AndroidClipboard.class).in(Singleton.class);
+      bind(android.content.ClipboardManager.class).toProvider(new SystemServiceProvider<android.content.ClipboardManager>(Context.CLIPBOARD_SERVICE));
+    }
   }
 }
