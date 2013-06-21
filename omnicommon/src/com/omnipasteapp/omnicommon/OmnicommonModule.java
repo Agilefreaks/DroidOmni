287276496f68a467d5fa0ea34c1bc51c4c2f19c3
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-06-21 11:18:46 +0300

    op-126 made omniservice transient

--- omnicommon/src/com/omnipasteapp/omnicommon/OmnicommonModule.java
+++ omnicommon/src/com/omnipasteapp/omnicommon/OmnicommonModule.java
@@ -10,7 +10,7 @@ import com.omnipasteapp.omnicommon.services.OmniService;
 public class OmnicommonModule extends AbstractModule {
   @Override
   protected void configure() {
-    bind(IOmniService.class).to(OmniService.class).in(Singleton.class);
+    bind(IOmniService.class).to(OmniService.class);
     bind(IConfigurationService.class).to(ConfigurationService.class).in(Singleton.class);
   }
 }
