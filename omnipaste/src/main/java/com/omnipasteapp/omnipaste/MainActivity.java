2165951a27a0d501b5bf7bbf5d77afdfc3ce36ee
calin <calinoiu.alexandru@agilefreaks.com>
2013-06-30 17:42:13 +0300

    extracts configuration in the correct places

--- omnipaste/src/main/java/com/omnipasteapp/omnipaste/MainActivity.java
+++ omnipaste/src/main/java/com/omnipasteapp/omnipaste/MainActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-import roboguice.activity.RoboActivity;
-
-public class MainActivity extends RoboActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
