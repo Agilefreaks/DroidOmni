1f025e0599171875246da1b8ba39ca776fa9a5a6
calin <calinoiu.alexandru@agilefreaks.com>
2013-07-03 10:21:59 +0300

    adds action sherlock support

--- omnipaste/src/main/java/com/omnipasteapp/omnipaste/MainActivity.java
+++ omnipaste/src/main/java/com/omnipasteapp/omnipaste/MainActivity.java
@@ -1,24 +1,23 @@
 package com.omnipasteapp.omnipaste;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
 import com.googlecode.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_main)
-public class MainActivity extends Activity {
+public class MainActivity extends SherlockActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
   }
 
-
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.main, menu);
+    getSupportMenuInflater().inflate(R.menu.main, menu);
     return true;
   }
 }
