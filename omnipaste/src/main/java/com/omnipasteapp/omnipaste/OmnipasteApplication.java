460002ea6bc9dc6ba12925de34f55212d80b52f3
calin <calinoiu.alexandru@agilefreaks.com>
2013-08-02 13:40:02 +0300

    extracts bugfreak configuration into strings.xml

--- omnipaste/src/main/java/com/omnipasteapp/omnipaste/OmnipasteApplication.java
+++ omnipaste/src/main/java/com/omnipasteapp/omnipaste/OmnipasteApplication.java
@@ -1,6 +1,7 @@
 package com.omnipasteapp.omnipaste;
 
 import android.app.Application;
+import android.content.res.Resources;
 
 import com.omnipasteapp.droidbugfreak.AgileReporter;
 import com.omnipasteapp.droidbugfreak.GlobalConfig;
@@ -15,9 +16,11 @@ public class OmnipasteApplication extends Application {
   public void onCreate() {
     super.onCreate();
 
-    GlobalConfig.Settings.setApiKey("apiKey");
-    GlobalConfig.Settings.setToken("token");
-    GlobalConfig.Settings.setServiceEndPoint("http://url.com");
+    Resources resources = this.getResources();
+
+    GlobalConfig.Settings.setApiKey(resources.getString(R.string.bugFreak_api_key));
+    GlobalConfig.Settings.setToken(resources.getString(R.string.bugFreak_token));
+    GlobalConfig.Settings.setServiceEndPoint(resources.getString(R.string.bugFreak_service_endpoint));
 
     AgileReporter.hook(this);
 
