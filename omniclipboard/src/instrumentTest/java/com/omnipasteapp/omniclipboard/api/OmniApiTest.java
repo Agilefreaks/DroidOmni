46227f97bfbf9ee4cb40b174aecddc0ab4f98f2c
Calin <calinoiu.alexandru@agilefreaks.com>
2013-08-21 10:59:30 +0300

    op-196 refactors the omni api

--- omniclipboard/src/instrumentTest/java/com/omnipasteapp/omniclipboard/api/OmniApiTest.java
+++ omniclipboard/src/instrumentTest/java/com/omnipasteapp/omniclipboard/api/OmniApiTest.java
@@ -1,12 +1,16 @@
 package com.omnipasteapp.omniclipboard.api;
 
+import android.content.Context;
+
 import junit.framework.TestCase;
+
+import static org.mockito.Mockito.mock;
 
 public class OmniApiTest extends TestCase {
   private IOmniApi subject;
 
   public void setUp() {
-    subject = new OmniApi();
+    subject = new OmniApi(mock(Context.class));
   }
 
   public void testClippingsReturnsAInstanceOfIClippings() {
