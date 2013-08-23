6998d622fa177b3da13a07a87bbeb2c28567ae94
Pirvu Doru Lucian <doru.pirvu@agilefreaks.com>
2013-08-23 18:12:37 +0300

    op-176 started work on storing clippings in sqlite

--- omnicommon/src/main/java/com/omnipasteapp/omnicommon/interfaces/IOmniService.java
+++ omnicommon/src/main/java/com/omnipasteapp/omnicommon/interfaces/IOmniService.java
@@ -1,10 +1,16 @@
 package com.omnipasteapp.omnicommon.interfaces;
 
+import com.omnipasteapp.omnicommon.domain.Clipping;
+
+import java.util.List;
+
 @SuppressWarnings("UnusedDeclaration")
 public interface IOmniService {
   public ILocalClipboard getLocalClipboard();
 
   public IOmniClipboard getOmniClipboard();
+
+  public List<Clipping> getClippings();
 
   public void start() throws InterruptedException;
 
