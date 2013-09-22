a9d9aa19d19ef445c000df8eaa743ff7724087f5
Calin <calinoiu.alexandru@agilefreaks.com>
2013-09-22 16:43:37 +0100

    op-242 patches a leak because of not closing AndroidHttpClient

--- omniclipboard/src/main/java/com/omnipasteapp/omniclipboard/api/AsyncRequestTask.java
+++ omniclipboard/src/main/java/com/omnipasteapp/omniclipboard/api/AsyncRequestTask.java
@@ -3,34 +3,34 @@ package com.omnipasteapp.omniclipboard.api;
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 
-import org.apache.http.client.HttpClient;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpUriRequest;
 
 public class AsyncRequestTask extends AsyncTask<Object, Void, Boolean> {
-
   private final HttpUriRequest request;
-  private final HttpClient client;
   private final ResponseHandler handler;
 
   public AsyncRequestTask(HttpUriRequest request, ResponseHandler handler) {
     this.request = request;
     this.handler = handler;
-
-    this.client = AndroidHttpClient.newInstance("DroidOmni");
   }
 
   @Override
   protected Boolean doInBackground(Object[] params) {
-    boolean executed;
+    HttpResponse response = null;
+    AndroidHttpClient client = AndroidHttpClient.newInstance("DroidOmni");
+
     try {
-      client.execute(request, handler);
-      executed = true;
+      response = client.execute(request);
+      handler.handleResponse(response);
     } catch (Throwable e) {
       e.printStackTrace();
-      executed = false;
+    }
+    finally {
+      client.close();
     }
 
-    return executed;
+    return response != null;
   }
 }
