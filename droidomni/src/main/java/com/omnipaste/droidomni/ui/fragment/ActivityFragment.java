196629f1bc2f2f1a94efd867a67d4f8cbcc47b05
calin <calinoiu.alexandru@agilefreaks.com>
2014-11-18 17:23:12 +0200

    [ao-165] adds swipe to refresh layout

--- droidomni/src/main/java/com/omnipaste/droidomni/ui/fragment/ActivityFragment.java
+++ droidomni/src/main/java/com/omnipaste/droidomni/ui/fragment/ActivityFragment.java
@@ -1,5 +1,6 @@
 package com.omnipaste.droidomni.ui.fragment;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,7 +12,14 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Inject;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 @EFragment(R.layout.fragment_activity)
 public class ActivityFragment extends BaseFragment<ActivityPresenter> {
@@ -20,6 +28,9 @@ public class ActivityFragment extends BaseFragment<ActivityPresenter> {
 
   @ViewById
   public RecyclerView list;
+
+  @ViewById
+  public SwipeRefreshLayout swipeRefreshLayout;
 
   @Override
   protected ActivityPresenter getPresenter() {
@@ -30,7 +41,28 @@ public class ActivityFragment extends BaseFragment<ActivityPresenter> {
   public void afterView() {
     list.setLayoutManager(new LinearLayoutManager(this.getActivity()));
     list.setItemAnimator(new DefaultItemAnimator());
-
     list.setAdapter(presenter.getClippingAdapter());
+
+    swipeRefreshLayout.setColorSchemeResources(
+        R.color.refresh_progress_1,
+        R.color.refresh_progress_2,
+        R.color.refresh_progress_3);
+    swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+      @Override public void onRefresh() {
+        swipeRefreshLayout.setRefreshing(true);
+
+        Observable
+            .timer(10, TimeUnit.SECONDS, Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                // onNext
+                new Action1<Long>() {
+                  @Override public void call(Long aLong) {
+                    swipeRefreshLayout.setRefreshing(false);
+                  }
+                });
+      }
+    });
+
   }
 }
