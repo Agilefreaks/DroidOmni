37832c7096a2111753425ffc78c8f7b11f064e98
calin <calinoiu.alexandru@agilefreaks.com>
2014-11-28 16:38:34 +0200

    [do-172] updates windows all alone fragment

--- droidomni/src/main/java/com/omnipaste/droidomni/presenter/TutorialClippingLocalPresenter.java
+++ droidomni/src/main/java/com/omnipaste/droidomni/presenter/TutorialClippingLocalPresenter.java
@@ -3,13 +3,8 @@ package com.omnipaste.droidomni.presenter;
 import com.omnipaste.droidomni.prefs.TutorialClippingLocal;
 import com.omnipaste.omnicommon.prefs.BooleanPreference;
 
-import java.util.concurrent.TimeUnit;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
-
-import rx.Observable;
-import rx.functions.Action1;
 
 @Singleton
 public class TutorialClippingLocalPresenter extends FragmentPresenter<TutorialClippingLocalPresenter.View> {
@@ -25,15 +20,8 @@ public class TutorialClippingLocalPresenter extends FragmentPresenter<TutorialCl
     this.tutorialClippingLocalWasPlayed = tutorialClippingLocalWasPlayed;
   }
 
+
   @Override public void initialize() {
-    Observable.timer(10, TimeUnit.SECONDS)
-        .observeOn(observeOnScheduler)
-        .subscribe(new Action1<Long>() {
-          @Override public void call(Long aLong) {
-            getView().close();
-            tutorialClippingLocalWasPlayed.set(true);
-          }
-        });
   }
 
   @Override public void resume() {
