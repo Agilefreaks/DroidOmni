e41265bc8ca6b441a06bc6606c7c0530f1bfa1a1
calin <calinoiu.alexandru@agilefreaks.com>
2015-03-05 11:53:03 +0200

    [do-217] fixes test

--- providerphone/src/test/java/com/omnipaste/phoneprovider/actions/SendSmsMessageRequestedTest.java
+++ providerphone/src/test/java/com/omnipaste/phoneprovider/actions/SendSmsMessageRequestedTest.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import rx.Observable;
 import rx.subjects.PublishSubject;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -31,20 +32,17 @@ public class SendSmsMessageRequestedTest {
 
   @Before
   public void context() {
-    sendSmsMessageRequested = new SendSmsMessageRequested(mockNotificationProvider);
+    sendSmsMessageRequested = new SendSmsMessageRequested(mockNotificationProvider, mockSmsMessages, mockSmsManager);
 
     when(mockNotificationProvider.getObservable()).thenReturn(Observable.<NotificationDto>empty());
     sendSmsMessageRequested.init("deviceId");
-
-    sendSmsMessageRequested.setSmsManager(mockSmsManager);
-    sendSmsMessageRequested.setSmsMessages(mockSmsMessages);
-
   }
 
   @Test
   public void executeWillSendMessageAndPatchApi() {
     PublishSubject<SmsMessageDto> smsMessagesObserver = PublishSubject.create();
     when(mockSmsMessages.get("42")).thenReturn(smsMessagesObserver);
+    when(mockSmsMessages.markAsSent(any(String.class), any(String.class))).thenReturn(Observable.<SmsMessageDto>empty());
 
     sendSmsMessageRequested.execute(new NotificationDto(NotificationDto.Type.SEND_SMS_MESSAGE_REQUESTED, "42"));
     smsMessagesObserver.onNext(new SmsMessageDto().setId("42").setPhoneNumber("+123").setContent("test"));
